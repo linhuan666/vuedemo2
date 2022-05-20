7842c74868058ac7f91839bc0b8d1c3f4fbc26ec
user_test <123@com>
2022-05-20 23:18:58 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <input type="text">{{msg}}
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data(){
+    return{
+      msg:"我是vue"
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+ background-color: red;
+}
+</style>
